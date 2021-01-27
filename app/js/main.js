93f54279ade3ff7e0e5ecc38671b23a0ef848cb1
Dencho96 <denysvk.96@gmail.com>
2021-01-27 14:20:37 +0200

    added responsive / bugfix

--- app/js/main.js
+++ app/js/main.js
@@ -16,13 +16,16 @@ $(".header__menu").click(function () {
 
 $('.header__menu').on('click', function () {
     $('.header__box').slideToggle();
-    $('.header__box').css('display','flex');
+    $('.header__box').css('display', 'flex');
 });
 
 var heroSlider = new Swiper('.hero__slider', {
     speed: 400,
     spaceBetween: 100,
     loop: "true",
+    autoplay: {
+        delay: 3000,
+    },
     pagination: {
         el: '.hero__pagination',
         type: 'bullets',
@@ -33,5 +36,39 @@ var partnersSlider = new Swiper('.partners__slider', {
     speed: 400,
     spaceBetween: 100,
     slidesPerView: 4,
-    loop: "true"
+    loop: "true",
+    navigation: {
+        nextEl: '.partners__btn--next',
+        prevEl: '.partners__btn--prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        576: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        992: {
+            slidesPerView: 4
+        }
+    }
+});
+
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+    $('.scroll-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
